88986518f0e1057b855056b6082fb65ec0be070a
chuangcaleb <balechuang@gmail.com>
2023-10-12 18:39:43 +0800

    chore: add sitemap and compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
+// import purgecss from "astro-purgecss";
 import { defineConfig } from "astro/config";
+
+const prodIntegrations = [
+  sitemap(),
+  compressor(),
+  // purgecss({
+  //   keyframes: true,
+  //   variables: false,
+  // }),
+];
+
+const integrations = [...(import.meta.env.PROD ? prodIntegrations : [])];
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.chuangcaleb.com",
   trailingSlash: "never",
+  integrations,
 });
