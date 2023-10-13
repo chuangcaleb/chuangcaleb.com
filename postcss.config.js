a6bc4d6bf49163914e9b3e16be9b4b0758c00f51
chuangcaleb <balechuang@gmail.com>
2023-10-13 10:06:17 +0800

    feat: scaffold hero section

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
 // postcss.config.js
+import OpenProps from "open-props";
+import postcssCustomMedia from "postcss-custom-media";
 import postcssJitProps from "postcss-jit-props";
 import postcssPresetEnv from "postcss-preset-env";
-import OpenProps from "open-props";
 
 const config = {
-  plugins: [postcssJitProps(OpenProps), postcssPresetEnv()],
+  plugins: [
+    postcssJitProps(OpenProps),
+    postcssPresetEnv(),
+    postcssCustomMedia(),
+  ],
 };
 
 export default config;
