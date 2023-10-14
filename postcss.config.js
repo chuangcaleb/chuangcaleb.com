799feaa89ca6257da0413fe99106484de562f73f
chuangcaleb <balechuang@gmail.com>
2023-10-14 17:19:17 +0800

    feat: some hero section restyling using PropPacks

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,9 @@ import postcssPresetEnv from "postcss-preset-env";
 
 const config = {
   plugins: [
-    postcssCustomMedia(),
     // postcssJitProps(OpenProps),
     // postcssPresetEnv({ stage: 3 }),
+    postcssCustomMedia(),
     postcssPresetEnv(),
   ],
 };
