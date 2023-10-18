347139ace44f3ea0a63eadfed6b43214f439c21e
chuangcaleb <balechuang@gmail.com>
2023-10-18 22:53:17 +0800

    refac: rename composeClasses utility to cn shorthand

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,4 +1,4 @@
-export function composeClasses(...classStrings: (string | null | undefined)[]) {
+export function cn(...classStrings: (string | null | undefined)[]) {
   const classStr = classStrings.filter(Boolean);
   if (!classStr.length) return null;
   return classStr.join(" ");
