50cc9ac00f68b266060e36942824fc5c238c1bdb
chuangcaleb <balechuang@gmail.com>
2023-10-20 12:01:33 +0800

    feat: recreate simple radix-like asChild slot architecture

---
+++ src/data/links.ts
@@ -0,0 +1,35 @@
+interface Link {
+  label: string;
+  ariaLabel: string;
+  href: string;
+  icon?: string;
+}
+
+const LINKS: { [s: string]: Link } = {
+  LINKEDIN: {
+    label: "LinkedIn",
+    href: "https://linkedin.com/in/chuangcaleb/",
+    ariaLabel: "LinkedIn profile of Chuang Caleb",
+    icon: "lucide:linkedin",
+  },
+  GITHUB: {
+    label: "GitHub",
+    href: "https://github.com/chuangcaleb",
+    ariaLabel: "GitHub profile of Chuang Caleb",
+    icon: "lucide:github",
+  },
+  CV: {
+    label: "CV / Résumé",
+    href: "https://docs.google.com/document/d/1qohsYe0w-Mq5yqIdNaHPuOHvs2lYvnGySULgR2Baqwk/edit?usp=sharing",
+    ariaLabel: "CV/Résumé of Chuang Caleb",
+    icon: "academicons:cv",
+  },
+  EMAIL: {
+    label: "Email",
+    href: "balechuang@gmail.com",
+    ariaLabel: "Email address of Chuang Caleb",
+    icon: "mdi:email-outline",
+  },
+} as const;
+
+export default LINKS;
--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,5 +1,15 @@
+import { parse } from "node-html-parser";
+
 export function cn(...classStrings: (string | null | undefined)[]) {
-  const classStr = classStrings.filter(Boolean);
-  if (!classStr.length) return null;
-  return classStr.join(" ");
+  const classes = classStrings.filter(Boolean);
+  if (!classes.length) return null;
+  return classes.join(" ");
+}
+
+export async function hs(htmlString: string, className: string | null) {
+  const root = parse(htmlString);
+  className && root.querySelector(":parent")?.classList.add(className);
+  // set yo own attributes
+  // we could iterate through each prop and set it manually, if it ever comes to that
+  return root.toString();
 }
