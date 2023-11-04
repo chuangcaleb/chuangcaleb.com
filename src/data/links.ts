07a1d91b6b04af4af230a6ca6555310545bb29a7
chuangcaleb <balechuang@gmail.com>
2023-11-04 13:43:54 +0800

    fix: remove aria-label for social links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,6 +1,5 @@
 interface Link {
   label: string;
-  ariaLabel: string;
   href: string | null;
   appId?: string;
   icon?: string;
@@ -10,28 +9,24 @@ const LINKS: { [s: string]: Link } = {
   LINKEDIN: {
     label: "LinkedIn",
     href: "https://linkedin.com/in/chuangcaleb/",
-    ariaLabel: "My LinkedIn profile, at the user ID of chuangcaleb",
     appId: 'chuangcaleb',
     icon: "mdi:linkedin",
   },
   GITHUB: {
     label: "GitHub",
     href: "https://github.com/chuangcaleb",
-    ariaLabel: "My GitHub profile, at the user ID of chuangcaleb",
     appId: 'chuangcaleb',
     icon: "mdi:github",
   },
   CV: {
     label: "CV / Résumé",
     href: "https://docs.google.com/document/d/1qohsYe0w-Mq5yqIdNaHPuOHvs2lYvnGySULgR2Baqwk/edit?usp=sharing",
-    ariaLabel: "CV / Résumé of Chuang Caleb",
     icon: "academicons:cv",
   },
   EMAIL: {
     label: "Email",
     href: 'mailto:ahoy@chuangcaleb.com',
     appId: 'ahoy@chuangcaleb.com',
-    ariaLabel: "Email address of Chuang Caleb",
     icon: "mdi:email-outline",
   },
 } as const;
