949e1f6fbd70d15f4d3471e50861984b7b223c96
chuangcaleb <balechuang@gmail.com>
2023-11-05 17:18:04 +0800

    clean: warn symbol in cloudinary/get.ts

--- lib/cloudinary/get.ts
+++ lib/cloudinary/get.ts
@@ -3,16 +3,15 @@ import getCache from "./reader";
 
 const cache = await getCache();
 
-export function getCldImgData(publicId: string) {
+export function getCldImgData(route: string) {
   if (!cache) throw new Error("Cloudinary resources have not been cached yet.");
 
-  const imgData = cache.find(
-    (resource) => resource.public_id === `${ROOT_DIR}/${publicId}`,
-  );
+  const publicId = `${ROOT_DIR}/${route}`;
+  const imgData = cache.find((resource) => resource.public_id === publicId);
 
   if (!imgData)
     console.warn(
-      `No cached Cloudinary resource found for public_id "${publicId}"`,
+      `⚠️ No cached Cloudinary resource found for public_id "${publicId}"`,
     );
 
   return imgData;
