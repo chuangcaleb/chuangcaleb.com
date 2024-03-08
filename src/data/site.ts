2c9a5283b045ebdf9f225e027c6a332e6a050686
chuangcaleb <balechuang@gmail.com>
2024-03-08 23:54:52 +0800

    fix: tally meta subtitle/description with page

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,8 @@
 export default {
   title: "Chuang Caleb",
-  subtitle: "Frontend Web Dev with a different story",
+  subtitle: "Software Dev with a Different Story",
   description:
-    "Malaysian guy who professionally tinkers with UI Design & dynamic web apps — and unprofessionally withstorytelling. You'll find out about me on this site. Wanna work together?",
+    "I professionally tinker with UI Design & dynamic web apps — and unprofessionally withstorytelling. You'll find out about me on this site. Wanna work together?",
   url: "https://chuangcaleb.com",
   language: "en",
   author: {
