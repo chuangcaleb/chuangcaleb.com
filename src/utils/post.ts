75bbdbdd7695722c6113951be3904d4306d85f0b
chuangcaleb <balechuang@gmail.com>
2024-03-12 23:50:19 +0800

    feat: toc!

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,13 +1,12 @@
 import { getCollection, type CollectionEntry } from "astro:content";
-import path from "path";
 import { slugify } from "~/utils/text";
 
 const DIR = "8 public";
-const SLUG_DIR = slugify(DIR);
+// const SLUG_DIR = slugify(DIR);
 
-export function isPublic(note: CollectionEntry<"obsidian-note">) {
-  return note.id.startsWith(DIR);
-}
+// export function isPublic(note: CollectionEntry<"obsidian-note">) {
+//   return note.id.startsWith(DIR);
+// }
 
 // function stripRootDir(string: string, rootDir: string) {
 //   return string.substring(rootDir.length + 1, string.length);
