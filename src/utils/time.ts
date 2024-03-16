2d0bec91789246ff560748c4f93c3a0c0150676c
chuangcaleb <balechuang@gmail.com>
2024-03-16 11:02:13 +0800

    feat: update getLongDurationSince function to use formatDuration consistently

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,6 @@
 import {
   differenceInDays,
+  differenceInMonths,
   format,
   formatDuration,
   intervalToDuration,
@@ -12,16 +13,20 @@ export function formatDisplayDate(datetime: number) {
 
 export function getLongDurationSince(datetime: number) {
   const then = new Date(datetime);
-  const days = differenceInDays(new Date(), then);
-  const duration = intervalToDuration({ start: then, end: new Date() });
+  const interval = intervalToDuration({ start: then, end: new Date() });
 
-  if (days < 365) {
-    const months = formatDuration({ months: duration?.months });
+  if (interval?.years) {
+    const years = formatDuration(interval, { format: ["years", "months"] });
+    return `${years} ago`;
+  }
+
+  if (interval?.months) {
+    const months = formatDuration(interval, { format: ["months"] });
     return `${months} ago`;
   }
 
-  const years = Number((days / 365).toFixed(1));
-  return `${formatDuration({ years })} ago`;
+  const days = formatDuration(interval, { format: ["days"] });
+  return `${days} ago`;
 }
 
 export function formatISO(date: string, template: string) {
