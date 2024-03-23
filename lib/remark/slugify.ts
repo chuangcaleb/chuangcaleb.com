265bb8562d55200f620cb61c419799e398a11779
chuangcaleb <balechuang@gmail.com>
2024-03-24 00:06:57 +0800

    feat: obsidian-style wikilinks

--- lib/remark/slugify.ts
+++ lib/remark/slugify.ts
@@ -3,16 +3,16 @@ export function removeConsecHyphens(string: string) {
 }
 
 // https://byby.dev/js-slugify-string
-// export function slugify(str: string) {
-//   return String(str)
-//     .normalize("NFKD") // split accented characters into their base characters and diacritical marks
-//     .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-//     .trim() // trim leading or trailing whitespace
-//     .toLowerCase() // convert to lowercase
-//     .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
-//     .replace(/\s+/g, "-") // replace spaces with hyphens
-//     .replace(/-+/g, "-"); // remove consecutive hyphens
-// }
+export function slugify(str: string) {
+  return String(str)
+    .normalize("NFKD") // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -/]/g, "") // remove non-alphanumeric characters
+    .replace(/\s+/g, "-") // replace spaces with hyphens
+    .replace(/-+/g, "-"); // remove consecutive hyphens
+}
 
 // https://dev.to/bybydev/how-to-slugify-a-string-in-javascript-4o9n
 // export function slugify(str: string) {
