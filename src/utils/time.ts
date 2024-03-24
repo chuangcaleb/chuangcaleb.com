9b4984ddf19709c6665a086381b9c9d911676863
chuangcaleb <balechuang@gmail.com>
2024-03-24 22:42:35 +0800

    feat: add Metadata component

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -7,12 +7,12 @@ import {
   parseISO,
 } from "date-fns";
 
-export function formatDisplayDate(datetime?: number) {
+export function formatDisplayDate(datetime?: string | Date) {
   if (!datetime) return null;
   return format(datetime, "dd MMMM yyyy");
 }
 
-export function getLongDurationSince(datetime: number) {
+export function getLongDurationSince(datetime: string | Date) {
   const then = new Date(datetime);
   const interval = intervalToDuration({ start: then, end: new Date() });
 
