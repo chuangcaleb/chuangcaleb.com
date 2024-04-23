75d3cb20d4248d4e24eab2800289ae66a5c545d9
chuangcaleb <balechuang@gmail.com>
2024-04-23 21:09:19 +0800

    fix: hotfix import issues in astro.config

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,18 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import { getPermalinks, remarkWikiLink } from "@portaljs/remark-wiki-link";
+// import { getPermalinks, remarkWikiLink } from "@portaljs/remark-wiki-link";
 import compressor from "astro-compressor";
 import purgecss from "astro-purgecss";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig, type AstroUserConfig } from "astro/config";
+// import { slugify } from "lib/markdown/slugify";
+// import { remarkReadingTime } from "lib/remark/reading-time";
+// import { remarkStripH1 } from "lib/remark/strip-h1";
 import icons from "unplugin-icons/vite";
-import { slugify } from "./lib/markdown/slugify";
-import { remarkReadingTime } from "./lib/remark/reading-time.mjs";
-import { remarkStripH1 } from "./lib/remark/strip-h1.mjs";
 import LINKS from "./src/data/links";
 
-const NOTES_DIR = "src/content/obsidian-note";
+// const NOTES_DIR = "src/content/obsidian-note";
 
 const prodIntegrations = [
   robotsTxt(),
@@ -54,17 +54,17 @@ export default defineConfig({
   markdown: {
     shikiConfig: { theme: "css-variables" },
     remarkPlugins: [
-      remarkStripH1,
-      remarkReadingTime,
-      [
-        remarkWikiLink,
-        {
-          pathFormat: "obsidian-short",
-          permalinks: getPermalinks(NOTES_DIR),
-          hrefTemplate: (permalink: string) =>
-            `/garden/note${slugify(permalink)}`,
-        },
-      ],
+      // remarkStripH1,
+      // remarkReadingTime,
+      // [
+      //   remarkWikiLink,
+      //   {
+      //     pathFormat: "obsidian-short",
+      //     permalinks: getPermalinks(NOTES_DIR),
+      //     hrefTemplate: (permalink: string) =>
+      //       `/garden/note${slugify(permalink)}`,
+      //   },
+      // ],
     ],
   },
   vite: {
