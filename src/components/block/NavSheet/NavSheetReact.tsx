c756a1e8d9fee57703d82b6763c300b2f8766857
chuangcaleb <balechuang@gmail.com>
2024-06-29 23:56:29 +0800

    feat: close sheet on anchor click

--- src/components/block/NavSheet/NavSheetReact.tsx
+++ src/components/block/NavSheet/NavSheetReact.tsx
@@ -2,30 +2,52 @@ import * as Dialog from "@radix-ui/react-dialog";
 import styles from "./styles.module.css";
 import { cn } from "~/utils/css";
 import { Fragment } from "react/jsx-runtime";
+import React from "react";
 
 // lazy any lol
-const DialogDemo = (props: any) => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <button className={styles.FloatingButton}>
-        <Fragment>{props.menuIcon}</Fragment>
-      </button>
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className={styles.DialogOverlay} />
-      <Dialog.Content className={cn(styles.DialogContent, "flow")}>
-        <Dialog.Title className={cn(styles.DialogTitle, "sr-only")}>
-          Page Navigation
-        </Dialog.Title>
-        <Dialog.Close asChild>
-          <button className={styles.CloseButton} aria-label="Close">
-            <Fragment>{props.closeIcon}</Fragment>
-          </button>
-        </Dialog.Close>
-        {props.body}
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+const NavSheetReact = (props: any) => {
+  const [open, setOpen] = React.useState(false);
 
-export default DialogDemo;
+  React.useEffect(() => {
+    window.addEventListener("hashchange", handleAClick, false);
+
+    return () => {
+      window.removeEventListener("hashchange", handleAClick, false);
+    };
+  }, []);
+
+  function handleAClick() {
+    setOpen(false);
+  }
+
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger asChild>
+        <button className={styles.FloatingButton}>
+          <Fragment>{props.menuIcon}</Fragment>
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className={styles.DialogOverlay} />
+        <Dialog.Content
+          className={cn(styles.DialogContent, "flow sheet-content")}
+        >
+          <Dialog.Title className={cn(styles.DialogTitle, "sr-only")}>
+            Page Navigation
+          </Dialog.Title>
+          <Dialog.Description className={"sr-only"}>
+            Navigation for the current page
+          </Dialog.Description>
+          <Dialog.Close asChild>
+            <button className={styles.CloseButton} aria-label="Close">
+              <Fragment>{props.closeIcon}</Fragment>
+            </button>
+          </Dialog.Close>
+          {props.body}
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
+
+export default NavSheetReact;
