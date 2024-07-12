a8b9dce50ae7f9d158288066bc67ca42ce2ce89e
chuangcaleb <balechuang@gmail.com>
2024-07-12 23:45:46 +0800

    feat: add guestbook

---
+++ lib/google-sheets/sheet.ts
@@ -0,0 +1,26 @@
+import process from 'node:process';
+import dotenv from 'dotenv';
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+import jwt from './jwt';
+import type {GuestPost} from './types';
+
+dotenv.config();
+
+export async function getGuestbookPosts() {
+	try {
+		const id = process.env.GUESTBOOK_GOOGLE_SHEET_ID;
+		if (!id) {
+			throw new Error('Missing GUESTBOOK_GOOGLE_SHEET_ID in .env');
+		}
+
+		const document = new GoogleSpreadsheet(id, jwt);
+
+		await document.loadInfo();
+		const sheet = document.sheetsByIndex[0];
+		const rows = await sheet.getRows();
+
+		return rows.map(r => r.toObject()) as GuestPost[];
+	} catch (error) {
+		throw error;
+	}
+}
