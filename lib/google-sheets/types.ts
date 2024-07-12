d8aca660d7faf1d40247f31965d2f1520e15bea5
Chuang Caleb <balechuang@gmail.com>
2024-07-12 23:55:38 +0800

    feat: add guestbook (#68)
    * feat: add guestbook
    
    * fix: env vars with line breaks
    
    Depending on the platform you use, sometimes setting/retreiving env vars that include line breaks can get tricky as they get encoded weirdly while saving these values in their system.
    
    https://stackoverflow.com/questions/74131595/error-error1e08010cdecoder-routinesunsupported-with-google-auth-library
    
    * feat: add guestbook to site nav

---
+++ lib/google-sheets/types.ts
@@ -0,0 +1,11 @@
+export type GuestPost = {
+	'Timestamp': string;
+	// 'Email address' ;
+	'📇 Display Name': string;
+	// '🤝 Real Name':;
+	'👥 Relation to me': string;
+	'🏷️ Tag': 'Work' | 'School' | 'Other';
+	'📃 Content Body': string;
+	'🔗 Social Link'?: string;
+	'📸 Avatar Image Link'?: string;
+};
