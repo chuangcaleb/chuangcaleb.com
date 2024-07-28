650a978ddc31313b8022dac74f48a4992d511c48
chuangcaleb <balechuang@gmail.com>
2024-07-28 17:58:19 +0800

    feat: update site title to chuangcaleb.com

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,5 +1,5 @@
 export default {
-	title: 'Chuang Caleb',
+	title: 'chuangcaleb.com',
 	subtitle: 'software dev with a different story',
 	description:
     'I professionally tinker with UI Design & dynamic web apps — and unprofessionally with storytelling. You\'ll find out about me on this site. Wanna work together?',
