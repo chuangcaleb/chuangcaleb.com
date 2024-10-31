aec27dba3552c0ddadfdac940e6f8ba74f92f247
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2024-10-31 19:25:50 +0800

    refac: start to redo the content config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,9 @@
 import {defineCollection, reference, z} from 'astro:content';
 
+// Two kinds of collections:
+// astro collections
+// obsidian collections, by caleb
+
 const projectCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
@@ -19,37 +23,42 @@ const projectCollection = defineCollection({
 	}),
 });
 
-const obsidianCollectionsCollection = defineCollection({
-	type: 'data',
-	schema: z.array(reference('obsidian-note')),
-});
+/* ------------------------------------ - ----------------------------------- */
+
+// const ma = z.discriminatedUnion('emojip', [
+// 	z.object({
+// 		emojip: z.string(),
+// 		series: z.boolean(),
+// 		collectionItems: z.array(reference('obsidian-note')),
+// 	}),
+// 	z.object({emojip: z.undefined()}),
+// ]);
+
+const baseObsidianNoteSchema = z
+	.object({
+		title: z.string(),
+		tags: z.array(z.string()).nullable(),
+		collection: z.array(reference('obsidian-note')),
+		prev: reference('obsidian-note'),
+		next: reference('obsidian-note'),
+		created: z.string(),
+		modified: z.string(),
+		published: z.string(),
+		featured: z.number(),
+		emojip: z.string(),
+		series: z.boolean(),
+		collectionItems: z.array(reference('obsidian-note')),
+	})
+	.partial();
 
 const obsidianNoteCollection = defineCollection({
 	type: 'content',
-	schema: z
-		.object({
-			title: z.string(),
-			tags: z.array(z.string()).nullable(),
-			collection: z.union([
-				reference('obsidian-note'), // FIXME: should be obsidian-collections
-				z.array(reference('obsidian-note')),
-			]),
-			prev: reference('obsidian-note'),
-			next: reference('obsidian-note'),
-			created: z.string(),
-			modified: z.string(),
-			published: z.string(),
-			series: z.boolean(),
-			collectionItems: z.array(reference('obsidian-note')),
-			emojip: z.string(),
-			featured: z.number(),
-		})
-		.partial(),
+	schema: baseObsidianNoteSchema,
+	// Schema: z.union([baseObsidianNoteSchema, ma]),
 });
-// Tags: z.array(z.string()).optional(),
 
 export const collections = {
 	project: projectCollection,
 	'obsidian-note': obsidianNoteCollection,
-	'obsidian-collection': obsidianCollectionsCollection,
 };
+
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+import type {getCollection} from 'astro:content';
+
+export type Note = Awaited<ReturnType<typeof getCollection<'obsidian-note'>>>[number];
