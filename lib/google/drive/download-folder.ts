1ebdb28ca8fc1525ab0be00e133e5bc875552b64
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2024-12-11 22:18:40 +0800

    fix: uncomment out everything, prod and prev env vars are kept separately lol

--- lib/google/drive/download-folder.ts
+++ lib/google/drive/download-folder.ts
@@ -15,77 +15,74 @@ const drive = google.drive({version: 'v3', auth: jwt});
 
 // get root id
 const rootId = process.env.GOOGLE_DRIVE_CONTENT_FOLDER_ID;
+if (!rootId) throw new Error(`Missing env: GOOGLE_DRIVE_CONTENT_FOLDER_ID`);
 
-console.log('🚀 ~ rootId:', process.env);
+// helper functions
+async function getFolderContents(id: string) {
+	const response = await drive.files.list({
+		q: `'${id}' in parents and trashed = false`,
+	});
+	return response.data.files;
+}
 
-// if (!rootId) throw new Error(`Missing env: GOOGLE_DRIVE_CONTENT_FOLDER_ID`);
+async function downloadFile(
+	fileName: string,
+	fileId: string,
+	filePath: string,
+) {
+	const response = await drive.files.get(
+		{fileId, alt: 'media'},
+		{responseType: 'stream'},
+	);
+	const destination = fs.createWriteStream(filePath);
+	await pipeline(response.data, destination);
+	console.info(`  ✅ ${fileName}`);
+}
 
-// // helper functions
-// async function getFolderContents(id: string) {
-// 	const response = await drive.files.list({
-// 		q: `'${id}' in parents and trashed = false`,
-// 	});
-// 	return response.data.files;
-// }
+// main function
+async function downloadFolder(folderId: string, destination: string) {
+	try {
+		// 1. Ensure the destination folder exists
+		if (!fs.existsSync(destination)) {
+			fs.mkdirSync(destination, {recursive: true});
+		}
 
-// async function downloadFile(
-// 	fileName: string,
-// 	fileId: string,
-// 	filePath: string,
-// ) {
-// 	const response = await drive.files.get(
-// 		{fileId, alt: 'media'},
-// 		{responseType: 'stream'},
-// 	);
-// 	const destination = fs.createWriteStream(filePath);
-// 	await pipeline(response.data, destination);
-// 	console.info(`  ✅ ${fileName}`);
-// }
+		// 2a. Get files in folder
+		const files = await getFolderContents(folderId);
+		// 2b. Handle empty folder
+		if (!files || files.length === 0) return;
 
-// // main function
-// async function downloadFolder(folderId: string, destination: string) {
-// 	try {
-// 		// 1. Ensure the destination folder exists
-// 		if (!fs.existsSync(destination)) {
-// 			fs.mkdirSync(destination, {recursive: true});
-// 		}
+		// 3. Iterate over folder children
+		for await (const file of files) {
+			if (typeof file.name !== 'string' || typeof file.id !== 'string') {
+				continue;
+			}
 
-// 		// 2a. Get files in folder
-// 		const files = await getFolderContents(folderId);
-// 		// 2b. Handle empty folder
-// 		if (!files || files.length === 0) return;
+			const filePath = path.join(destination, file.name);
 
-// 		// 3. Iterate over folder children
-// 		for await (const file of files) {
-// 			if (typeof file.name !== 'string' || typeof file.id !== 'string') {
-// 				continue;
-// 			}
+			// 3a. If subfolder, recurse
+			if (file.mimeType === 'application/vnd.google-apps.folder') {
+				// If it's a subfolder, recursively download it
+				await downloadFolder(file.id, filePath);
+				continue;
+			}
 
-// 			const filePath = path.join(destination, file.name);
+			// 3b. If file, then download
+			await downloadFile(file.name, file.id, filePath);
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error('🚨 Error:', error.message);
+			return;
+		}
 
-// 			// 3a. If subfolder, recurse
-// 			if (file.mimeType === 'application/vnd.google-apps.folder') {
-// 				// If it's a subfolder, recursively download it
-// 				await downloadFolder(file.id, filePath);
-// 				continue;
-// 			}
+		console.error('🚨 Error', error);
+	}
+}
 
-// 			// 3b. If file, then download
-// 			await downloadFile(file.name, file.id, filePath);
-// 		}
-// 	} catch (error) {
-// 		if (error instanceof Error) {
-// 			console.error('🚨 Error:', error.message);
-// 			return;
-// 		}
-
-// 		console.error('🚨 Error', error);
-// 	}
-// }
-
-// // entry
-// const destination = 'src/content/obsidian-note';
-// console.info(
-// 	`📥 Beginning to download Google Drive folder "${rootId}" to "${destination}"`,
-// );
-// await downloadFolder(rootId, destination);
+// entry
+const destination = 'src/content/obsidian-note';
+console.info(
+	`📥 Beginning to download Google Drive folder "${rootId}" to "${destination}"`,
+);
+await downloadFolder(rootId, destination);
