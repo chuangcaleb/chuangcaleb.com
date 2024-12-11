c632ef574b9032c06ef5645d716e71f4d68c95c6
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2024-12-11 18:16:22 +0800

    format: fix xo lint/formatting!! wahoooo

--- .xo-config.cjs
+++ .xo-config.cjs
@@ -2,7 +2,6 @@ module.exports = {
   rules: {
     // turn it off until i fix it
     "@typescript-eslint/naming-convention": "off",
-    "capitalized-comments": "off",
     // fix conflict between XO(import/extensions) and XO(n/file-extension-in-import)."never".
     "import/extensions": [
       "error",
