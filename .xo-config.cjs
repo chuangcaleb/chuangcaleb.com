163d25ad7d6af781daa52dc67f6616e7ea40b118
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2024-12-11 20:28:54 +0800

    chore: disable eslint/capitalized-comments

--- .xo-config.cjs
+++ .xo-config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     // turn it off until i fix it
     "@typescript-eslint/naming-convention": "off",
+    "capitalized-comments": "off",
     // fix conflict between XO(import/extensions) and XO(n/file-extension-in-import)."never".
     "import/extensions": [
       "error",
