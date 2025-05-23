d96c808a27edee30ccc93e2af2e87c4279396c16
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-05-23 21:32:38 +0800

    perf: hash favicon with src/assets

--- src/pages/site.webmanifest.ts
+++ src/pages/site.webmanifest.ts
@@ -1,6 +1,6 @@
 import type {APIContext} from 'astro';
-import android_chrome_192x192 from 'public/assets/favicon/android-chrome-192x192.png';
-import android_chrome_512x512 from 'public/assets/favicon/android-chrome-512x512.png';
+import android_chrome_192x192 from 'src/assets/favicon/android-chrome-192x192.png';
+import android_chrome_512x512 from 'src/assets/favicon/android-chrome-512x512.png';
 import site from '~/data/site';
 
 const icons = [
