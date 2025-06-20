3e9a610fb5e67e1694cac2d899bcd78c3be7a109
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-06-20 21:04:53 +0800

    refactor: FUNKY external arrows WAHOOO

--- lib/utils/markdown.ts
+++
@@ -1,4 +0,0 @@
-import type {AstroComponentFactory} from 'astro/runtime/server/index.js';
-import ProseA from '~/components/styled/monom/ProseA.astro';
-
-export const defaultComponents = {a: ProseA as AstroComponentFactory};
