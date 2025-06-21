d2d4807afa759920548a6135b18bf313bb5f30e0
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-06-21 22:05:26 +0800

    feat: VIEW TRANSITIONS, BABYYYY

--- src/components/block/NavSheet/nav-sheet-react.tsx
+++ src/components/block/NavSheet/nav-sheet-react.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import React from 'react';
+import React, {useEffect, type MouseEvent} from 'react';
 import {Fragment} from 'react/jsx-runtime';
 import styles from './styles.module.css';
 import {cn} from '~/utils/css.ts';
@@ -13,17 +13,41 @@ const NavSheetReact = (properties: any) => {
 	const {scrollY, direction} = useScroll('up');
 	const {width} = useWindowSize();
 
-	React.useEffect(() => {
-		globalThis.addEventListener('hashchange', handleAClick, false);
+	const sheetRef = React.useRef<HTMLDivElement>(null);
 
+	// Previously should use an eventlistener for `hashchange`
+	// but this clashes with ViewTransitions API
+	useEffect(() => {
+		function handleClick(event: Event): void {
+			if (!(event instanceof MouseEvent)) return;
+
+			const {target} = event;
+			if (!(target instanceof Element)) return;
+			if (!sheetRef.current?.contains(target)) return;
+
+			const anchor = target.closest('a');
+			if (!(anchor instanceof HTMLAnchorElement)) return;
+
+			const href = anchor.getAttribute('href');
+			if (!href?.startsWith('#')) return;
+
+			event.preventDefault();
+			setOpen(false);
+
+			setTimeout(() => {
+				const scrollTarget = document.querySelector(href);
+				if (scrollTarget instanceof HTMLElement) {
+					scrollTarget.scrollIntoView({behavior: 'smooth'});
+					history.replaceState(null, '', href);
+				}
+			}, 300);
+		}
+
+		document.addEventListener('click', handleClick);
 		return () => {
-			globalThis.removeEventListener('hashchange', handleAClick, false);
+			document.removeEventListener('click', handleClick);
 		};
 	}, []);
-
-	function handleAClick() {
-		setOpen(false);
-	}
 
 	const isMobileTop = width && width < 900 && scrollY < 1;
 	const shouldShowFloatingButton = direction === 'down' || isMobileTop;
@@ -48,6 +72,7 @@ const NavSheetReact = (properties: any) => {
 				<Dialog.Overlay className={styles.DialogOverlay} />
 				<Dialog.Content
 					className={cn(styles.DialogContent, 'flow sheet-content')}
+					ref={sheetRef}
 				>
 					<Dialog.Title className={cn(styles.DialogTitle, 'sr-only')}>
 						Page Navigation
