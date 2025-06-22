3bff753a8c0d1efb7a3604063c3eaba085508df3
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-06-22 15:40:08 +0800

    feat: exclude description from metadata table

--- lib/obsidian/consts.ts
+++ lib/obsidian/consts.ts
@@ -1,3 +1,9 @@
 // export const LINK_KEYS = ['collection', 'up', 'next', 'prev'];
 
-export const EXCLUDE_KEYS = ['collectionItems', 'tags', 'series', 'resources'];
+export const EXCLUDE_KEYS = [
+	'collectionItems',
+	'tags',
+	'series',
+	'resources',
+	'description',
+];
