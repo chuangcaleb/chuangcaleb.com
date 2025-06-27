e103d3ca462a8dfd9601ebd9feea3a3089ddbf6b
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-06-27 12:03:18 +0800

    refactor: reuse NoteMetadata

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,8 +1,8 @@
 import type {CollectionEntry, ReferenceDataEntry} from 'astro:content';
 
 export type NoteReference = ReferenceDataEntry<'obsidian-note'>;
-
 export type Note = CollectionEntry<'obsidian-note'>;
+export type NoteMetadata = Note['data'];
 
 export type SuperNote = CollectionEntry<'obsidian-note'> & {
 	name: string;
