7edd1224f6c4691fed73d87ebfa81d778793da2e
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-06-28 10:21:11 +0800

    feat: add fallback meta description for notes route

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,6 +2,8 @@ export default {
 	title: 'chuangcaleb.com',
 	subtitle: 'software dev with a different story',
 	description: "Chuang Caleb's web-portfolio and public digital garden.",
+	siteDescriptionFallback:
+		"A note from Chuang Caleb's public digital garden, where content is fluid and alive.",
 	language: 'en',
 	author: {
 		name: 'Chuang Caleb',
