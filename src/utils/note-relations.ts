f00769d0cb9ffa1b8865e209ca2d60d04bbaba7d
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-06-28 18:40:01 +0800

    feat: add parent notes into NoteRelationsSection

--- src/utils/note-relations.ts
+++ src/utils/note-relations.ts
@@ -2,25 +2,28 @@ import type {NoteMetadata} from 'lib/utils/types';
 import {getNoteEntries} from './note.ts';
 
 export async function formatNoteRelations(metadata: NoteMetadata) {
-	// (childrenNotes || !!seriesesNotes.length) && (
 	const {down: children = [], up: parents = [], series} = metadata;
 
 	const parentNotes = await getNoteEntries(parents);
 
 	const isParent = Boolean(children?.length);
-	const childrenNotes = isParent && {
-		isOrdered: Boolean(series),
-		items: series ?? children,
-	};
+	const childrenNotes = isParent
+		? {
+				isOrdered: Boolean(series),
+				items: series ?? children,
+			}
+		: null;
 
-	// parents which are also series
-	// type SeriesesNotes = Series[]; type Series = SeriesMember[]
-	const seriesesNotes = parentNotes.filter((note) => Boolean(note.data.series));
-	// const regularParentsNotes = parentNotes.filter((note) => !note.data.series);
+	const type = (() => {
+		if (metadata.series) return 'topic/series';
+		if (metadata.emojip) return 'topic';
+		return 'regular';
+	})();
 
-	if (!childrenNotes || !seriesesNotes) return null;
+	if (!childrenNotes && !parentNotes) return null;
 	return {
 		children: childrenNotes,
-		serieses: seriesesNotes,
-	};
+		parents: parentNotes,
+		type,
+	} as const;
 }
